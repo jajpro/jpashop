364a1344c965425c56d57f54e9691fee0e25d2fd
jej <jajpro7884@gmail.com>
2021-01-02 22:29:25 +0900

    ver2: Entity tuned

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -25,10 +25,16 @@ public class Category {
     ) //관계형 DB 는 중간관계를 나타내는 Table 필요. 실무에서는 안씀. 중간 Table 에 다른 값을 넣을 수 없음. 따라서 설계 시 새로 1:N, N:1 로 풀어서 처리
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 매서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne  //Many order by one member. 연관관계의 주인
+    @ManyToOne(fetch = FetchType.LAZY)  //Many order by one member. 연관관계의 주인
     @JoinColumn(name = "member_id") //FK 가 member_id 가 되어 맵핑
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,5 +34,28 @@ public class Order {
 
     @Enumerated(EnumType.STRING) //ORDINAL 은 숫자 방식인데 쓰면안됨. 중간에 다른 상태가 들어가면 XXX 로 되버림
     private OrderStatus status; //주문상태 [Order, Cancel]
+
+    //==연관관계 매서드 : 양쪽 세팅을 원자적으로 한 곳에서 해버릴 수 있도록 함==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+        /*
+        public static void main(String args[]) {
+            Member member = new Member();
+            Order order = new Order();
+            //하나로 줄일 수 있음
+            //member.getOrders().add(order);
+            order.setMember(member);
+        }*/
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
